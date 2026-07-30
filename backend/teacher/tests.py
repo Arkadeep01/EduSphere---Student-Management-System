cf13bd6f42bdd022b056120567e6bd7fd80e8a25
Arkadeep01 <arjundevrath001@gmail.com>
2026-07-30 21:21:26 +0530

    feat(teacher): update teacher tests

--- backend/teacher/tests.py
+++ backend/teacher/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from rest_framework.test import APIClient
 from rest_framework import status
 
